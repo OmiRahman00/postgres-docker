e8f2021fe89c554328138343c11b70a9fa92c386
OmiRahman00 <aromi.fs@bulipetech.com>
2025-06-16 12:17:56 +0600

    adding practices from the office until the partioning

--- Important query.sql
+++ Important query.sql
@@ -1,11 +1,11 @@
- //it's for  query analysis it returnes query execution time
+--  //it's for  query analysis it returnes query execution time
  explain analyze select id from employees where id =1000;
- // it's for  query analysis it returnes query execution time and it's slow because it's doesn't have any index
+--  // it's for  query analysis it returnes query execution time and it's slow because it's doesn't have any index
  explain analyze select name from employees where name = 'Zs';
 
  //
  explain analyze select id,name from employees where name like '%Zs%';
- //create index on name column
+--  //create index on name column
  create index employees_name on employees(name);
  
  explain analyze select id, g from students where g > 80 and g < 95 order by g desc;
---
+++ docker-commands.md
@@ -0,0 +1,5 @@
+docker exec -it pg psql -U postgres
+
+//show the database table structure
+
+\d grades_org
